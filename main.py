4677be9a93e37e9e4270a440065d71f1faf151ac
Lim Kha Shing <kslim5703@gmail.com>
2022-04-05 19:40:43 +0800

    add __main__ function

--- main.py
+++ main.py
@@ -12,49 +12,55 @@ pytesseract.pytesseract.tesseract_cmd = r'D:/Tesseract-OCR/tesseract.exe'
 image_size = [600, 700, 800]
 regex_found = False
 
-for size in image_size:
-    img = cv2.imread(r'./cleaned/cleaned.jpg')
-    img = cv2.resize(img, (size,size), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    ocr_result = pytesseract.image_to_string(img).upper()
-    # print(raw_results)
-    # print("=====================================")
+def main():
+    for size in image_size:
+        img = cv2.imread(r'./cleaned/cleaned.jpg')
+        img = cv2.resize(img, (size,size), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    results = ocr_result.split()
-    # print(results)
-    # print("=====================================")
+        ocr_result = pytesseract.image_to_string(img).upper()
+        # print(raw_results)
+        # print("=====================================")
 
-    # check ic
-    for result in results:
-        regex_found = re.search(IC_NUMBER_REGREX, result)
-        if bool(regex_found):
-            break
-    if any(pattern in results for pattern in IC_PATTERNS) or bool(regex_found):
-        print("IS IC")
-        break
+        results = ocr_result.split()
+        # print(results)
+        # print("=====================================")
 
-    # check driving, with regrex Date and IC Number
-    for result in results:
-        regex_found = re.search(DRIVING_DATE_REGREX, result)
-        if bool(regex_found):
+        # check ic
+        for result in results:
+            regex_found = re.search(IC_NUMBER_REGREX, result)
+            if bool(regex_found):
+                break
+        if any(pattern in results for pattern in IC_PATTERNS) or bool(regex_found):
+            print("IS IC")
             break
 
-        regex_found = re.search(DRIVING_IC_NUMBER_REGREX, result)
-        if bool(regex_found):
-            break
-    if any(pattern in results for pattern in DRIVING_PATTERN) or bool(regex_found):
-        print("IS DRIVING")
-        break
+        # check driving, with regrex Date and IC Number
+        for result in results:
+            regex_found = re.search(DRIVING_DATE_REGREX, result)
+            if bool(regex_found):
+                break
 
-    # check passport
-    for result in results:
-        regex_found = re.search(PASSPORT_DATE_REGREX, result)
-        if bool(regex_found):
+            regex_found = re.search(DRIVING_IC_NUMBER_REGREX, result)
+            if bool(regex_found):
+                break
+        if any(pattern in results for pattern in DRIVING_PATTERN) or bool(regex_found):
+            print("IS DRIVING")
             break
-    if any(pattern in results for pattern in PASSPORT_PATTERNS) or bool(regex_found):
-        print("IS PASSPORT")
-        break
 
-    # cv2.imshow('image', img)
-    # cv2.waitKey()
+        # check passport
+        for result in results:
+            regex_found = re.search(PASSPORT_DATE_REGREX, result)
+            if bool(regex_found):
+                break
+        if any(pattern in results for pattern in PASSPORT_PATTERNS) or bool(regex_found):
+            print("IS PASSPORT")
+            break
+
+        # cv2.imshow('image', img)
+        # cv2.waitKey()
+
+
+if __name__ == '__main__':
+    main()
